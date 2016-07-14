69fd23aa24ccca72090536ca160180448134b2a5
Tan Jun Yen <tanjunyen@gmail.com>
2016-07-14 11:34:36 +0200

    Refactoring.

---
+++ src/main/java/com/junyen/dev/Box.java
@@ -0,0 +1,22 @@
+package com.junyen.dev;
+
+/**
+ * Created by tanjunyen on 14/07/16.
+ */
+final class Box {
+    private final int width;
+    private final int height;
+
+    public Box(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
--- src/main/java/com/junyen/dev/TicTacToe.java
+++ src/main/java/com/junyen/dev/TicTacToe.java
@@ -6,45 +6,51 @@ import java.awt.*;
 
 public class TicTacToe {
     private JFrame frame;
+    private JPanel contentPane;
 
     public TicTacToe(String frameName) {
         this.frame = new JFrame(frameName);
+        this.contentPane = new JPanel();
     }
 
     public void initializeBoard() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(3, 3));
+        contentPane.setLayout(new GridLayout(3, 3));
+        frame.setContentPane(contentPane);
 
-        frame.setContentPane(panel);
-
-        int contentPaneWidth = frame.getContentPane().getWidth();
-        int boxWidth = (contentPaneWidth / 3) - 50;
-        int contentPaneHeight = frame.getContentPane().getHeight();
-        int boxHeight = (contentPaneHeight / 3) - 50;
-
-        int NB_OF_BOXES = 3;
-        int xPosition = 10;
-        int yPosition = 10;
-        for (int i = 0; i < NB_OF_BOXES; i++) { //for column
-            for (int y = 0; y < NB_OF_BOXES; y++) { //for row
-                Rectangle rectangle = new Rectangle(xPosition, yPosition);
-                frame.getContentPane().add(rectangle);
-                xPosition += boxWidth + 10;
-            }
-            yPosition += boxHeight + 10;
-        }
+        Box box = initializeBox();
+        placeBoxesOnGameBoard(box);
 
         frame.setVisible(true);
         frame.pack();
     }
 
-    public void setBoardSize(int width, int height) {
-        frame.setSize(width, height);
+    private Box initializeBox() {
+        int boxWidth = contentPane.getWidth() / 3;
+        int boxHeight = contentPane.getHeight() / 3;
+        Box box = new Box(boxWidth, boxHeight);
+        return box;
+    }
+
+    private void placeBoxesOnGameBoard(Box box) {
+        final int NB_OF_BOXES = 3;
+        final int GAP = 10;
+        int xPosition = GAP;
+        int yPosition = GAP;
+
+        for (int i = 0; i < NB_OF_BOXES; i++) { //for column
+            for (int y = 0; y < NB_OF_BOXES; y++) { //for row
+                Rectangle rectangle = new Rectangle(xPosition, yPosition);
+                frame.getContentPane().add(rectangle);
+                xPosition += box.getWidth() + GAP;
+            }
+            yPosition += box.getHeight() + GAP;
+        }
     }
 
     public void initializeFrame() {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(500, 500));
+        frame.setPreferredSize(new Dimension(400, 400));
+        frame.setMaximumSize(new Dimension(600, 600));
         frame.pack();
     }
 }
