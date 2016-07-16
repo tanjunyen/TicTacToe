d3dc8402924784184cb93827dc31e5f5c1485625
Tan Jun Yen <tanjunyen@gmail.com>
2016-07-16 21:34:30 +0200

    Implemented drawing of rectangle or circle on mouse click.

--- src/main/java/com/junyen/dev/App.java
+++ src/main/java/com/junyen/dev/App.java
@@ -2,12 +2,18 @@ package com.junyen.dev;
 
 import com.junyen.dev.swing.GameWindow;
 
+import javax.swing.*;
+
 public class App
 {
     public static void main( String[] args )
     {
-        GameWindow app = new GameWindow("GameWindow");
-        app.initializeFrame();
-        app.initializeBoard();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                GameWindow app = new GameWindow("GameWindow");
+                app.initializeFrame();
+                app.initializeBoard();
+            }
+        });
     }
 }
--- src/main/java/com/junyen/dev/swing/Square.java
+++ src/main/java/com/junyen/dev/swing/Square.java
@@ -2,18 +2,60 @@ package com.junyen.dev.swing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class Square extends JPanel {
-    private final int WIDTH = 200;
-    private final int HEIGHT = 200;
+
+    private char token = ' ';
+    private static int turn = 0;
 
     public Square() {
+        setBorder(BorderFactory.createLineBorder(Color.black));
+        addMouseListener(new boxMouseListener());
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.blue);
-        g.drawRect(0, 0, WIDTH, HEIGHT);
+        if (token == 'X') {
+            g.drawLine(10, 10, this.getWidth() - 10, this.getHeight() - 10);
+            g.drawLine(this.getHeight() - 10, 10, 10, this.getHeight() - 10);
+        } else if (token == 'O') {
+            g.drawOval(10, 10, this.getWidth() - 20, this.getHeight() - 20);
+        }
+    }
+
+    public void setToken(char token) {
+        this.token = token;
+    }
+
+    private class boxMouseListener implements MouseListener {
+        public void mouseClicked(MouseEvent e) {
+            System.out.println("Box is clicked!");
+            if (turn % 2 == 0) {
+                setToken('X');
+            } else {
+                setToken('O');
+            }
+            turn++;
+            repaint();
+        }
+
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 }
